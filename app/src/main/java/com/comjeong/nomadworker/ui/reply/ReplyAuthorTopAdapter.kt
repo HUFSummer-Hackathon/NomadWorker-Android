e96338a0286bfb51ee92fc8236b2691a009004f4
JaesungLeee <viki0114@naver.com>
2022-08-09 17:41:58 +0900

    [UI] #61 댓글 UI 구현

---
+++ app/src/main/java/com/comjeong/nomadworker/ui/reply/ReplyAuthorTopAdapter.kt
@@ -0,0 +1,47 @@
+package com.comjeong.nomadworker.ui.reply
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.comjeong.nomadworker.databinding.ItemFeedReplyAuthorBinding
+
+/**
+ * @author 이재성
+ *
+ * 기본 Adapter 틀은 만들어 놨고 Author에 해당하는 Model 넣어서 하면 됨
+ * 변경해야 할 부분
+ * 1. Model
+ * 2. ViewHolder
+ * 3. DiffCallback
+ */
+
+class ReplyAuthorTopAdapter : ListAdapter<Any, ReplyAuthorTopAdapter.ReplyAuthorTopViewHolder>(ReplyOthersDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReplyAuthorTopViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFeedReplyAuthorBinding.inflate(inflater, parent, false)
+        return ReplyAuthorTopViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReplyAuthorTopViewHolder, position: Int) {
+        holder.bindItems(getItem(position))
+    }
+
+    class ReplyAuthorTopViewHolder(private val binding: ItemFeedReplyAuthorBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindItems(item: Any?) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    class ReplyOthersDiffCallback : DiffUtil.ItemCallback<Any>() {
+        override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
+            return true
+        }
+    }
+}
