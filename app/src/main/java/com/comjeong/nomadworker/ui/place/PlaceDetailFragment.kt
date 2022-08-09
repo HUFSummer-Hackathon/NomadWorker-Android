8676adfe6f2b246a0480318bff6a15c2d7c62e32
Yoon-Min <yoonmin0113@naver.com>
2022-08-09 17:32:05 +0900

    [UI] #62 장소 상세에 평점 추가하기 기능 레이아웃 구현

--- app/src/main/java/com/comjeong/nomadworker/ui/place/PlaceDetailFragment.kt
+++ app/src/main/java/com/comjeong/nomadworker/ui/place/PlaceDetailFragment.kt
@@ -1,9 +1,14 @@
 package com.comjeong.nomadworker.ui.place
 
+import android.app.AlertDialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.comjeong.nomadworker.R
 import com.comjeong.nomadworker.common.Constants.CAMERA_ZOOM
 import com.comjeong.nomadworker.common.Constants.PLACE_ID_KEY
@@ -25,7 +30,6 @@ class PlaceDetailFragment : BaseFragment<FragmentPlaceDetailBinding>(R.layout.fr
     private val viewModel: PlaceDetailViewModel by viewModel()
     private var placeLatitude: Double = 0.0
     private var placeLongitude: Double = 0.0
-
 
     private lateinit var mGoogleMap: GoogleMap
     private lateinit var mMapView: MapView
@@ -50,10 +54,13 @@ class PlaceDetailFragment : BaseFragment<FragmentPlaceDetailBinding>(R.layout.fr
 
         bindViews()
         observePlaceDetailInfo()
+        bindEvaluationClick()
 
         mMapView = binding.mvPlaceMap
         mMapView.onCreate(savedInstanceState)
         mMapView.getMapAsync(this)
+
+
     }
 
     private fun observePlaceDetailInfo() {
@@ -93,4 +100,54 @@ class PlaceDetailFragment : BaseFragment<FragmentPlaceDetailBinding>(R.layout.fr
     private fun getPlaceLocation(): LatLng {
         return LatLng(placeLatitude, placeLongitude)
     }
+
+    private fun bindEvaluationClick() {
+        binding.btnEvaluation.setOnClickListener {
+            setEvaluationDialog()
+        }
+    }
+
+    private fun setEvaluationDialog() {
+        val builder = AlertDialog.Builder(requireActivity(),R.style.AlertDialogTheme)
+        val view = LayoutInflater.from(requireActivity()).inflate(R.layout.dialog_evaluation,
+            requireActivity().findViewById<ConstraintLayout>(R.id.dialog_layout))
+
+        builder.setView(view)
+
+        val placeName =  view.findViewById<TextView>(R.id.tv_place_name)
+        val ratingNumber = view.findViewById<TextView>(R.id.tv_evaluation_number)
+        val alertMessage = view.findViewById<TextView>(R.id.tv_alert)
+        val evaluationButton =  view.findViewById<Button>(R.id.btn_add_evaluation)
+        val closeButton = view.findViewById<Button>(R.id.btn_close)
+        val ratingBar = view.findViewById<RatingBar>(R.id.rb_rating)
+        placeName.text = binding.tvPlaceName.text
+        ratingNumber.text = ratingBar.rating.toString()
+
+        val alertDialog = builder.create()
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+             ratingNumber.text = rating.toString()
+        }
+
+        evaluationButton.setOnClickListener {
+            if(ratingBar.rating == 0f){
+                alertMessage.visibility = View.VISIBLE
+            }
+            else{
+                // TODO( 서버에 평점 바뀐 것을 전송 및 업데이트 된 평점을 뷰에 적용 )
+                alertDialog.dismiss()
+            }
+        }
+
+        closeButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        if(alertDialog.window != null){
+            alertDialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+        }
+
+        alertDialog.show()
+    }
+
 }
